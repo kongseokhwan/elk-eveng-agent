bc1f2fa71ec813db584288db2c49b6d8b72f7c04
kongseokhwan@gmail.com <kongseokhwan@gmail.com>
2017-10-26 09:56:46 +0900

    ACL Actuator update
    ACL Actuator update

--- actions/kulcloud/py-beem/__main__.py
+++ actions/kulcloud/py-beem/__main__.py
@@ -2,6 +2,11 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 
+# wsk -i action invoke beem --blocking --result --param device_name "se_prism" 
+# --param controller "35.187.147.69:7171" --param acl_number "11" --param match_order "deny" --param src_host "1.0.0.11" 
+# --param src_mask "0.0.0.255" --param protocol "ip" --param dst_host "" --param dst_mask ""
+
+
 def beem(params):
     """
     @brief      { acl api client module }
@@ -10,10 +15,10 @@ def beem(params):
     @param      params['acl_number']                string: one of them (acl, reboot)
     @param      params['match_order']                string: device ip
     @param      params['src_host']            string: source ip
-    @param      params['src_wildcard']       string: source netmask
+    @param      params['src_mask']       string: source netmask
     @param      params['protocol']          string: protocol
     @param      params['dst_host']            string: destination ip  
-    @param      params['dst_wildcard']       string: destination netmask 
+    @param      params['dst_mask']       string: destination netmask 
 
     
     @return     { "message": "string" }
@@ -24,19 +29,19 @@ def beem(params):
     acl_number = params['acl_number']
     match_order = params['match_order']
     src_host = params['src_host']
-    src_wildcard = params['src_wildcard']
+    src_wildcard = params['src_mask']
     protocol = params['protocol']
     dst_host = params['dst_host']
-    dst_wildcard = params['dst_wildcard']     
+    dst_wildcard = params['dst_mask']     
 
     body = {
         "acl_number": acl_number,
         "match_order": match_order,
         "src_host": src_host,
-        "src_wildcard": src_wildcard,
+        "src_mask": src_wildcard,
         "protocol": protocol,
         "dst_host": dst_host,
-        "dst_wildcard": dst_wildcard
+        "dst_mask": dst_wildcard
     }
 
     try:
@@ -45,24 +50,6 @@ def beem(params):
     except
         RuntimeError as e:
         LOG.error("%s SMTP Agent terminated!", e)
-        return {
-            "acl_number": acl_number,
-            "match_order": match_order,
-            "src_host": src_host,
-            "src_wildcard": src_wildcard,
-            "protocol": protocol,
-            "dst_host": dst_host,
-            "dst_wildcard": dst_wildcard,
-            "message": "ACL Send Error"
-        }
+        return params
 
-    return {
-            "acl_number": acl_number,
-            "match_order": match_order,
-            "src_host": src_host,
-            "src_wildcard": src_wildcard,
-            "protocol": protocol,
-            "dst_host": dst_host,
-            "dst_wildcard": dst_wildcard,
-            "message": "ACL Send Suceess"
-        }
+    return params
